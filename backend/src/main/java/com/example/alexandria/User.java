b6cfbc567724bcf937d5c7fa8ee09b234c6159cc
Nikita <thekillermaps@gmail.com>
2023-09-28 15:49:05 +0300

    back-end

---
+++ backend/src/main/java/com/example/alexandria/User.java
@@ -0,0 +1,17 @@
+package com.example.alexandria;
+import javax.persistence.*;
+import lombok.*;
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String group;
+}
---
+++ backend/src/main/java/com/example/alexandria/UserRepository.java
@@ -0,0 +1,4 @@
+package com.example.alexandria;
+
+public interface UserRepository {
+}
