1ea4fa067db9d139e92dcfc25ddcd7c3de0baf1d
Илья Свиридов <ilya.sviridov.05@gmail.com>
2023-11-02 12:40:52 +0100

    schedule

--- frontend/src/app/components/base/schedule/schedule.component.ts
+++ frontend/src/app/components/base/schedule/schedule.component.ts
@@ -12,28 +12,55 @@ export class ScheduleComponent implements OnInit {
     schedule: ScheduleItem[] = [];
     user: UserDTO | null = null;
 
-    constructor(private scheduleService: ScheduleService) {
-    }
+    constructor(private scheduleService: ScheduleService) { }
+    //////// /////////////мое
+    weekNumber: number = 1; // Инициализируем значение номера недели
 
     ngOnInit(): void {
         this.user = JSON.parse(localStorage.getItem('user') || 'null');
         console.log(this.user);
         let groupName = this.user?.group_name;
-        this.scheduleService.getScheduleByGroup(groupName).subscribe((data: ScheduleItem[]) => {
-            this.schedule = data.sort((a, b) => {
-                // Сначала сортируем по дням недели
-                if (a.day_of_week > b.day_of_week) return -1;
-                if (a.day_of_week < b.day_of_week) return 1;
-
-                // Если дни недели одинаковые, сортируем по времени начала
-                if (a.lessonId.start_time < b.lessonId.start_time) return -1;
-                if (a.lessonId.start_time > b.lessonId.start_time) return 1;
-
-                return 0;  // Если и дни недели, и время начала одинаковые
-            });
-
-            console.log(data);
+        // Используйте ваш сервис для получения расписания по выбранной группе
+        this.scheduleService.getScheduleByGroup(groupName).subscribe((data : ScheduleItem[]) => {
+            this.schedule = data;
         });
+        // пофиксить //////////////////////////////////////
+        const today = new Date();
+        const dayOfWeek = today.getDay();
 
+        if (dayOfWeek === 5) { // Если сегодня пятница
+            const dayOfMonth = today.getDate();
+            this.weekNumber = (dayOfMonth % 2 === 0) ? 2 : 1;
+        }
+    }
+
+
+    isRedColor(dayIndex: number): boolean {
+        const today = new Date().getDay();
+        return (today === dayIndex + 1);
+    }
+
+    getColumnColor(dayIndex: number): object {
+        const today = new Date().getDay();
+        const color = (today === dayIndex + 1) ? 'red' : 'rgba(147, 147, 147, 1)';
+        
+        return { 'background-color': color};
+    }
+
+    //Пофиксить время пары, а то шото не то.
+    getTimeSlotStyle(index: number): object {
+        const now = new Date();
+
+        const intervals = [
+            { startTime: new Date('2023-11-01T08:00:00'), endTime: new Date('2023-11-01T12:00:00'), color: 'purple' },
+            { startTime: new Date('2023-11-01T14:00:00'), endTime: new Date('2023-11-01T18:00:00'), color: 'purple' },
+            { startTime: new Date('23:50:00'), endTime: new Date('01:00:00'), color: 'purple' },
+        ];
+
+        const currentInterval = intervals.find(interval => now >= interval.startTime && now <= interval.endTime);
+
+        const color = currentInterval ? currentInterval.color : 'rgba(82, 135, 173, 1)';
+
+        return { 'background-color': (index % 2 === 0) ? color : 'rgba(82, 135, 173, 1)' }; // применяем цвет к четным элементам
     }
 }
