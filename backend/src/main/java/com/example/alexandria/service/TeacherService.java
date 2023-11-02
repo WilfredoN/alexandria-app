eb885c2cc971a4c56d94be319199c968fffc3d63
Nikita <thekillermaps@gmail.com>
2023-11-02 14:53:09 +0200

    clear imports backend and fixed group creating in db

--- backend/src/main/java/com/example/alexandria/service/TeacherService.java
+++ backend/src/main/java/com/example/alexandria/service/TeacherService.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
