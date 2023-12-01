c97a27554b646c9911a1c45b492c5e8ce1bd121d
Nikita <59343780+WilfredoN@users.noreply.github.com>
2023-12-01 12:33:45 +0200

    Update confirm-dialog.component.ts

--- frontend/src/app/components/base/profile/confirm-dialog.component.ts
+++ frontend/src/app/components/base/profile/confirm-dialog.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'confirm-dialog',
   templateUrl: './confirm-dialog.component.html',
-  styleUrls: ['./confirm-dialog.component.css']
 })
 export class ConfirmDialogComponent {
   
